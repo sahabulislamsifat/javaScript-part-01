2c6e501efc2158d21ddb961993e5b1663f47194d
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-15 02:06:55 +0600

    exploring js number and number methods

---
+++ number.js
@@ -0,0 +1,23 @@
+// let a = "10";
+// let a = "10e-5";
+// let a = "10-5";
+// let b = 10;
+
+// console.log(isNaN(a));
+// console.log(isNaN(b));
+
+// const five = "5";
+// const two = 2;
+
+// const text = `Five + Two = ${five + two} but Five - Two ${five - two}`;
+
+// console.log(text);
+
+// let x = 123;
+// console.log((123 + 5).toString());
+// console.log(typeof (123 + 5).toString());
+
+// let newN = x.toString();
+
+// console.log(newN);
+// console.log(typeof newN);
