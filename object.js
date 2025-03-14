21b407fd0aa0dbd59561795a84e105d1c7e390bb
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-14 18:06:11 +0600

    js String

--- object.js
+++ object.js
@@ -30,26 +30,3 @@
 
 // console.log(shape.diameter());
 
-// const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-// console.log(letter.length);
-
-// console.log("Hello \tWorld");
-
-// let text = "The quick brown fox jumps over the lazy dog.";
-
-// console.log(text.length);
-
-// let x = "Sifat";
-// let y = new String("Sifat");
-
-// console.log(x == y);
-// console.log(x === y);
-
-const about = "My name is Sifat. I am 22 years old.";
-
-// console.log(name.length);
-// console.log(about.slice(11, 16));
-// console.log(about.slice(-36, -18));
-
-console.log(about.substring(16, 10));
---
+++ string.js
@@ -0,0 +1,61 @@
+// const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// console.log(letter.length);
+
+// console.log("Hello \tWorld");
+
+// let text = "The quick brown fox jumps over the lazy dog.";
+
+// console.log(text.length);
+
+// let x = "Sifat";
+// let y = new String("Sifat");
+
+// console.log(x == y);
+// console.log(x === y);
+
+// const about = "My name is Sifat. I am 22 years old.";
+
+// console.log(name.length);
+// console.log(about.slice(11, 16));
+// console.log(about.slice(-36, -18));
+
+// console.log(about.substring(16, 10));
+
+// Replacing String Content
+
+// let string = "I love Traveling";
+// let string = "I love Traveling & Traveling";
+
+// let replace = string.replace("Traveling", "Sleeping");
+
+// console.log(string);
+// console.log(replace);
+
+// Converting to Upper to Lower Case
+// let sentence = "Hello World";
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1.concat(" ", text2);
+// let text3 = text1.concat(" - ", text2);
+// let sentence2 = sentence.toUpperCase();
+// let sentence2 = sentence.toLowerCase();
+
+// console.log(sentence2);
+// console.log(text3);
+
+// let text = "      Hello World      ";
+let text = "Hello World";
+// let text2 = text.trim();
+
+// console.log(text);
+// console.log(text2);
+
+// console.log(text[0]);
+// console.log(typeof text.charAt([500]));
+// console.log(typeof text[500]);
+
+console.log(text.split(" "));
+console.log(text.split(","));
+console.log(text.split());
+console.log(text.split(""));
