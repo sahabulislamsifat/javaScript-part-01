f070d0541c1b3544f781a40be323195f04ba8d5f
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-14 17:40:54 +0600

    explore js object and string with testing console

---
+++ object.js
@@ -0,0 +1,55 @@
+// const sayHi = () => {
+//   console.log(name);
+//   console.log(age);
+//   var name = "John";
+//   let age = 25;
+// };
+
+// sayHi();
+
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+// }
+
+// alert(person.lastName);
+// console.log(person.lastName)
+// document.write(person.lastName);
+// console.log(person["lastName"]);
+// document.innerHTML(person.lastName);
+// alert(person[lastName]);
+// alert(person["lastName"]);
+// console.log(person[lastName]
+
+// const shape = {
+//   radius: 10,
+//   diameter() {
+//     return this.radius * 2;
+//   },
+// };
+
+// console.log(shape.diameter());
+
+// const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// console.log(letter.length);
+
+// console.log("Hello \tWorld");
+
+// let text = "The quick brown fox jumps over the lazy dog.";
+
+// console.log(text.length);
+
+// let x = "Sifat";
+// let y = new String("Sifat");
+
+// console.log(x == y);
+// console.log(x === y);
+
+const about = "My name is Sifat. I am 22 years old.";
+
+// console.log(name.length);
+// console.log(about.slice(11, 16));
+// console.log(about.slice(-36, -18));
+
+console.log(about.substring(16, 10));
---
+++ test.js
@@ -0,0 +1,21 @@
+// {
+//     let var = 'Hello World'
+// }
+
+// console.log(var);
+
+// CONST
+// let a = 5;
+// let a = 5;
+// a = 8;
+// console.log(a);
+
+// const a = 5;
+// const a = 5;
+// a = 10;
+
+// console.log(a);
+
+let x = 29 % 9;
+
+console.log(x);
